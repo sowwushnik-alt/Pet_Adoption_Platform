183da8b63a4452b128df16fd391ca05d2585c8c7
sowwushnik-alt <253653@astanait.edu.kz>
2025-12-19 00:59:03 +0500

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        Pet pet1 = new Pet("Muiza", "Cat", 3);
+        Pet pet1 = new Pet("Kasian", "Cat", 3);
         Pet pet2 = new Pet("Basian", "Cat", 2);
 
 
